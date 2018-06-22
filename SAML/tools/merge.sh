e2d7992e670da1a3e6c9e06c62ddf4ab91d2cb0c
Doug Harris <doug.harris@xsilosystems.com>
2018-06-22 15:43:02 -0400

    Reformat the requirements vertically
    The arangement of Kantara requirements and their CATS
    applicability  as horizontal rows in a table is visually problematic when the requirement text is longer than a few sentences.
    
    Reformatting ther information vertically should make it easier
    to read.

--- SAML/tools/merge.sh
+++ SAML/tools/merge.sh
@@ -4,38 +4,43 @@
 REGEX="^\[.+]::"
 
 function startTable() {
-    # Start table
-    echo '[cols="4a,1a,2a"]'
-    echo '|==='
-    echo '|Kantara Requirement|CATS Status|CATS Details'  
+    : # Start table
+    # echo '[cols="4a,1a,2a"]'
+    # echo '|==='
+    # echo '|Kantara Requirement|CATS Status|CATS Details'  
 }
 
-function endRow() {
-    echo '|'$CATS_STATUS
-    echo '|'$CATS_DETAILS
-}
-
-function endTable {
-    endRow
-    echo '|==='
+function endRequirement() {
+    echo '===='
+    if [[ $CATS_STATUS == 'Supported' ]] ; then
+        echo
+        echo '*CATS Support*: _Required_'
+        echo
+    else
+        echo
+        echo '*CATS Support*: _Constrained_'
+        echo
+        echo $CATS_DETAILS
+        echo
+    fi
+    echo "''''"
 }
 
 
 REQUIREMENT=
 while read SOURCE_LINE; do
    if [[ $SOURCE_LINE =~ ^= && -n $REQUIREMENT ]] ; then
-      endTable
+      endRequirement
       REQUIREMENT=
    fi
 
    if [[ $SOURCE_LINE =~ $REGEX ]] ; then # New Requirement
-      if [[ -z $REQUIREMENT ]] ; then 
-         startTable
-      else # Finish previous requirement
-         endRow
+      if [[ -n $REQUIREMENT ]] ; then 
+         endRequirement
       fi
 
       REQUIREMENT=$(echo $SOURCE_LINE | cut -f 1 -d ':' | tr -d '[]')
+      SOURCE_LINE=${SOURCE_LINE#*:: }
       if  [[ -f ./src/constraints/$REQUIREMENT.adoc ]] ; then #CATS Constraints
          CATS_STATUS='Constrained'
          CATS_DETAILS=$(cat ./src/constraints/$REQUIREMENT.adoc)
@@ -43,13 +48,16 @@ while read SOURCE_LINE; do
          CATS_STATUS='Supported'
          CATS_DETAILS=
       fi
-
-      echo '|'
+      echo
+      echo '*Kantara Requirement*: *_'$REQUIREMENT'_*'
+      echo
+      echo '===='
+      echo
   fi
    
    echo "$SOURCE_LINE"
 done
 
 if [[ -n $REQUIREMENT ]] ; then
-   endTable
+   endRequirement
 fi
